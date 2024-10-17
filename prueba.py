1dbccee6a519a4652849413fc69afccbac3e8e46
jjrojasan <jehoverr@gmail.com>
2024-10-16 19:27:35 -0500

    Se agrego la Rama1 y se cambio Rama1

--- prueba.py
+++ prueba.py
@@ -1,1 +1,1 @@
-print("Cambios Hola grupo vamos a ver como cambiar el nombre de un archivo y copmo eliminarlo ")
+print("Cambios de Rama1 cambiada  Hola grupo vamos a ver como cambiar el nombre de un archivo y copmo eliminarlo ")
